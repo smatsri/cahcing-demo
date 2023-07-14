924df8cea4a0f16f057431f3779e17cb4fd9dfbb
Shai <smatsri@gmail.com>
2023-07-14 18:21:20 +0300

    added logs + init todos

--- lib/todos/api.ts
+++ lib/todos/api.ts
@@ -1,4 +1,6 @@
-const getTodo = async (id: number) => {
+import { Todo } from "./types"
+
+export const getTodo = async (id: number) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
   const todo: Todo = await response.json()
   return todo
--- server.ts
+++ server.ts
@@ -2,37 +2,49 @@ import next from 'next'
 import nextBuild from 'next/dist/build'
 import path from 'path'
 import express from 'express'
+import { initTodos } from './lib/todos/init'
+import logger from './lib/utils/logger'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 const start = async (): Promise<void> => {
-  if (process.env.NEXT_BUILD) {
-    app.listen(PORT, async () => {
-      console.info(`Next.js is now building...`)
-      // @ts-expect-error
-      await nextBuild(path.join(__dirname, '../'))
-      process.exit()
+  try {
+    logger.info('Starting server...')
+    const success = await initTodos()
+    if (!success) {
+      throw new Error('Failed to initialize todos');
+    }
+
+    if (process.env.NEXT_BUILD) {
+      app.listen(PORT, async () => {
+        logger.info('Next.js is now building...')
+        // @ts-expect-error
+        await nextBuild(path.join(__dirname, '../'))
+        process.exit()
+      })
+
+      return
+    }
+
+    const nextApp = next({
+      dev: process.env.NODE_ENV !== 'production',
     })
 
-    return
+    const nextHandler = nextApp.getRequestHandler()
+
+    app.get('*', (req: any, res: any) => nextHandler(req, res))
+
+    nextApp.prepare().then(() => {
+      logger.info('Next.js started')
+
+      app.listen(PORT, () => {
+        logger.info(`Listening on port ${PORT}`)
+      })
+    })
+  } catch (error) {
+    logger.error('Error occurred during startup:', error);
   }
-
-  const nextApp = next({
-    dev: process.env.NODE_ENV !== 'production',
-  })
-
-  const nextHandler = nextApp.getRequestHandler()
-
-  app.get('*', (req: any, res: any) => nextHandler(req, res))
-
-  nextApp.prepare().then(() => {
-    console.info('Next.js started')
-
-    app.listen(PORT, async () => {
-      console.info(`listening on port ${PORT}`)
-    })
-  })
 }
 
 start()
